a2ccb51c304af967df1a855fa4470492e3b9e03a
Manuel Bautista <b0hcoder@gmail.com>
2017-05-11 15:24:08 -0400

    fix for the QS Reports

--- Scripts/ReportQSCADownloadReport.js
+++ Scripts/ReportQSCADownloadReport.js
@@ -3,7 +3,7 @@
     //$(".tab-styledButton.left")[0].click();
     setTimeout(function () {
         var frame = document.getElementsByTagName("iframe")[0];
-        frame.contentWindow.document.getElementsByClassName("tab-styledButton left")[0].click();
+        frame.contentWindow.document.getElementsByTagName('a')[0].click();
         //$(".tab-styledButton.left")[0].click();
         //alert(frame.contentWindow.document);
     }, 2000); //2 Segundos
